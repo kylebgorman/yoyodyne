23b4614cb5610a0f6adf2b7d986bac5eb5d206c8
Kyle Gorman <kylebgorman@gmail.com>
2024-12-04 00:19:27 -0500

    New beam search module. Not yet integrated

---
+++ yoyodyne/models/beam_search.py
@@ -0,0 +1,141 @@
+"""Beam search classes.
+
+Current limitations:
+
+* Beam search uses Python's heap implementation; this is reasonably performant
+  in cPython (it uses a C extension module where available) but there may be a
+  better pure PyTorch solution.
+* Beam search assumes a batch size of 1; it is not clear how to extend it to
+  larger batches.
+* We work with log-likelihoods exclusively and thus addition of two log
+  probabilities is equivalent to multiplication of real numbers.
+* Beam search is designed to support RNN and attentive RNN models; it has not
+  yet been generalized for other model types.
+* Not much attention has been paid to keeping things on device, under the
+  hypothesis this is mostly on CPU anyways.
+
+Sample usage:
+
+    def beam_decode(
+        self, encoder_output: torch.Tensor, encoder_mask: torch.Tensor
+    ) -> torch.Tensor:
+        beam = Beam([self._initial_input(self.batch_size)]))))
+        for t in range(max_length):
+            with BeamHelper(self.beam_width) as helper:
+                for cell in beam.cells:
+                    decoder_input = cell.decoder_output.to(self.device)
+                    decoder_output = self.decode_one(decoder_input)
+                    helper.record(cell, decoder_outout)
+            beam = helper.update()
+            if beam.final:
+                break
+        predictions = nn.utils.rnn.pad_sequence(
+            [torch.tensor(cell.symbols) for cell in beam.cells],
+            padding_value=special.PAD_IDX,
+            device=self.device,
+        ).unsqueeze(0)
+        loglikes = torch.tensor(
+            [cell.loglike) for cell in beam.cells],
+            device=self.device,
+        )
+        return predictions, loglikes
+"""
+
+from __future__ import annotations
+
+import dataclasses
+import heapq
+import operator
+
+from typing import Iterator, List
+
+from torch import nn
+
+from . import base
+from .. import special
+
+
+class BeamCell:
+    """Represents a (partial) hypotheses in the beam search.
+
+    Args:
+        decoder_input (base.ModuleOutput).
+    """
+
+    decoder_input: base.ModuleOutput
+    # List of symbols in the hypothesis.
+    symbols: List[int]
+    # Log-likelihood of the hypothesis.
+    loglike: float
+
+    def __init__(self, decoder_input: base.ModuleOutput):
+        self.decoder_input = decoder_input
+        self.symbols = [special.START_IDX]
+        self.loglike = 0.0
+
+    def extensions(
+        self, decoder_output: base.ModuleOutput
+    ) -> Iterator[BeamCell]:
+        if self.final:
+            # No extensions are possible.
+            yield self
+            return
+        logits = decoder_output.output.squeeze((0, 1))
+        predictions = nn.functional.log_softmax(logits, dim=0).cpu()
+        for last_symbol, log_prob in enumerate(predictions):
+            yield BeamCell(
+                decoder_output,
+                self.symbols + [last_symbol],
+                self.loglike + log_prob,
+            )
+
+    @property
+    def final(self) -> bool:
+        return self.symbols[-1] == special.END_IDX
+
+
+class Beam:
+    """Represents the current state of the beam."""
+
+    cells: List[BeamCell]
+
+    def __init__(self, cells: List[BeamCell], beam_width: int):
+        self.cells = cells
+
+    def __len__(self) -> int:
+        return len(self.cells)
+
+    @property
+    def final(self) -> bool:
+        return all(cell.final for cell in self.cells)
+
+
+@dataclasses.dataclass
+class BeamHelper:
+    """Helps extend a beam."""
+
+    beam_width: int
+    heap: List[BeamCell] = []
+
+    def __enter__(self) -> BeamHelper:
+        return self
+
+    # TODO: test if operator.itemgetter is faster on dataclasses.
+    beam_key = operator.attrgettr("loglike")
+
+    def record(
+        self, cell: BeamCell, decoder_output: base.ModuleOutput
+    ) -> None:
+        for new_cell in cell.extensions(decoder_output):
+            if len(self.new_beam) < self.beam_width:
+                heapq.heappush(self.heap, new_cell, key=self.beam_key)
+            else:
+                heapq.heappushpop(self.heap, new_cell, key=self.beam_key)
+
+    def __exit__(self):
+        # Sorts hypotheses so the minimum log-likelihood is the first
+        # element; we think this is faster than calling heapq.nlargest.
+        self.heap.sort(key=self.beam_key, reverse=True)
+
+    def update(self) -> Beam:
+        return Beam(self.heap)
